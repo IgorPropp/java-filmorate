90993b7dafb5a1748c64887e874537f556dc53b4
IgorPropp <120092187+IgorPropp@users.noreply.github.com>
2023-12-08 19:02:13 +0500

    Спринт 9 Исправления чекстайл

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -14,7 +14,7 @@ import java.util.List;
 @RestController
 public class FilmController {
 
-    private final HashMap<Integer, Film> films = new HashMap<>();
+    private final HashMap <Integer, Film> films = new HashMap<>();
     private int filmId = 1;
 
     @GetMapping("/films")
